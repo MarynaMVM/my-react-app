4f7fb68765b892b5c42407124f988091fcfd91af
MarynaMVM <104380960+MarynaMVM@users.noreply.github.com>
2022-10-04 16:55:28 +0200

    updated API key

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,18 +2,5 @@ import React from "react";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
-  return (
-    <div className="weatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="day">Thu</div>
-          <h5>icon</h5>
-          <div>
-            <span className="max-temp">15°</span>
-            <span className="min-temp">10°</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <h7>...</h7>;
 }
