d1b9ed49a6540121298a118837323f5e5bd89c28
MarynaMVM <104380960+MarynaMVM@users.noreply.github.com>
2022-10-10 12:08:52 +0200

    removed Weather comp

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./App.css";
-import Weather from "./Weather";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
